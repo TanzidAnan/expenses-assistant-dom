5dd53e572a956f1463449a88c2f93afb9716552a
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-11 23:12:31 +0600

    value

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 
 document.getElementById('calculate').addEventListener('click',function(){
     const expenseForm =document.getElementById('income').value;
+    const software =document.getElementById('software').value;
     console.log(56656);
     console.log(parseInt(expenseForm) )
+    console.log(parseInt(software) )
 })
