bf444fa8b0d678aa54a5ba7a1583f505dce1723e
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-11 23:22:16 +0600

    parseint value

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 
 
-document.getElementById('calculate').addEventListener('click',function(){
-    const expenseForm =document.getElementById('income').value;
-    const software =document.getElementById('software').value;
-    const courses =document.getElementById('courses').value;
-    const internet =document.getElementById('internet').value;
-    console.log(parseInt(expenseForm) )
-    console.log(parseInt(software) )
-    console.log(parseInt(courses) )
-    console.log(parseInt(internet) )
+document.getElementById('calculate').addEventListener('click', function () {
+    const expenseForm = document.getElementById('income').value;
+    const software = document.getElementById('software').value;
+    const courses = document.getElementById('courses').value;
+    const internet = document.getElementById('internet').value;
+    const totleElement = parseInt(software + courses + internet);
+    const softwarevalue =parseInt(software)
+    const coursesvalue =parseInt(courses)
+    const internetvalue =parseInt(internet)
+    if (expenseForm < totleElement) {
+        console.log(softwarevalue)
+        console.log(coursesvalue)
+        console.log(internetvalue)
+    }
 })
